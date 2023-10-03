05ab700232245fb6124ed7e62ee3f902ccfcc1ff
pcarbonero <pcarbonero@106-19.info2>
2023-10-03 14:24:29 +0200

    [NEW] ej 5

---
+++ Tema2/src/IfElse/Ejercicio5.java
@@ -0,0 +1,28 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// variables
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Dime otro número ");
+		num2 = sc.nextInt();
+		
+		System.out.println("Dime otro número ");
+		num3 = sc.nextInt();
+		
+		if (num1 < num2 && num1 < num3 && num2 < num3) {
+			System.out.println(num1 + "<" + num2 + "<" + num3);
+		}
+		else if (num1 == num2 && num2 == num3) {
+			System.out.println("Son iguales ");
+		}
+	}
+	}
