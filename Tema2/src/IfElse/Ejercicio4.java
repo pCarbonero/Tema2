d0a4b139b0c36f13969891517f62443bfc10d49f
pcarbonero <pcarbonero@106-19.info2>
2023-10-03 14:19:14 +0200

    [UPD] ej 4 caso son iguales

--- Tema2/src/IfElse/Ejercicio4.java
+++ Tema2/src/IfElse/Ejercicio4.java
@@ -15,11 +15,14 @@ public class Ejercicio4 {
 		System.out.println("Dime otro número ");
 		num2 = sc.nextInt();
 		
-		if (num1 < num2) {
+		if (num1 < num2 && num1 != num2) {
 			System.out.println(num1 + "<" + num2);
 		}
-		else {
+		else if (num1 > num2 && num1 != num2){
 			System.out.println(num2 + "<" + num1);
+		}		
+		else if (num1 == num2) {
+			System.out.println("Son iguales ");
 		}
 	}
 
