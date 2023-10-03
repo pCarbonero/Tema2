a4446e9a5b3bab37879077a2f8a92170e374edd2
pcarbonero <pcarbonero@106-19.info2>
2023-10-03 14:05:20 +0200

    [NEW] ej 4

---
+++ Tema2/src/IfElse/Ejercicio4.java
@@ -0,0 +1,26 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// variables
+		int num1, num2;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Dime otro número ");
+		num2 = sc.nextInt();
+		
+		if (num1 < num2) {
+			System.out.println(num1 + "<" + num2);
+		}
+		else {
+			System.out.println(num2 + "<" + num1);
+		}
+	}
+
+}
