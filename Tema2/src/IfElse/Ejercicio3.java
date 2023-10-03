d2591021208e4be89a7cf92ddbe2110383e741d5
pcarbonero <pcarbonero@106-19.info2>
2023-10-03 13:55:14 +0200

    [NEW] ej 3

---
+++ Tema2/src/IfElse/Ejercicio3.java
@@ -0,0 +1,25 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// Variables
+		float num;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número decimal ");
+		num = sc.nextFloat();
+		
+		if (num!=1 && num!=-1 && num!=0 &&(num>-1 && num<1)) {
+			System.out.println("ES CASI 0");
+		}
+		else {
+			System.out.println("NO ES");
+		}
+		
+		sc.close();
+	}
+
+}
