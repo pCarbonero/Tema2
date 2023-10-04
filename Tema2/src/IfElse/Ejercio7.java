939ce5a7292e364b2e91ee6c086c7e61c63dc12b
pcarbonero <pcarbonero@106-19.info2>
2023-10-04 09:22:42 +0200

    [NEW] ej 7

---
+++ Tema2/src/IfElse/Ejercio7.java
@@ -0,0 +1,35 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class Ejercio7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num, res;
+		String chain;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número entre 0 y 99.999 ");
+		num = sc.nextInt();
+		
+		if (num >= 0 && num <= 99999) {
+								
+			chain = Integer.toString(num);
+			res = chain.length();
+			
+			if (num<10) {
+				System.out.println("Tu número tiene " + res + " cifra");
+			}
+			else {
+				System.out.println("Tu número tiene " + res + " cifras");			
+			}
+		}
+		else {
+			System.out.println("Tu número no está entre 0 y 99999!! ");
+		}
+
+		sc.close();
+	}
+
+}
