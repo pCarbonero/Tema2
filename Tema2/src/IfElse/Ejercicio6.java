41f07f181c69646498c684d11163c6a286507643
pcarbonero <pcarbonero@106-19.info2>
2023-10-04 09:10:11 +0200

    [NEW] ej 6

--- Tema2/src/IfElse/Ejercicio6.java
+++ Tema2/src/IfElse/Ejercicio6.java
@@ -7,7 +7,7 @@ public class Ejercicio6 {
 	public static void main(String[] args) {
 		// variables
 		double a, b, c;
-		double x;
+		double x1, x2;
 		Scanner sc = new Scanner(System.in); 
 		
 		System.out.println("dame un valor para a ");
@@ -17,7 +17,18 @@ public class Ejercicio6 {
 		System.out.println("dame un valor para c ");
 		c = sc.nextInt();
 		
-		x=(-b + Math.sqrt((Math.pow(b, 2)-4*a*c)));
+		if ((Math.pow(b, 2)-4*a*c) >= 0 && 2*a != 0) {
+			
+			x1=(-b + Math.sqrt((Math.pow(b, 2)-4*a*c)))/(2*a);
+			x2=(-b - Math.sqrt((Math.pow(b, 2)-4*a*c)))/(2*a);	
+			
+			System.out.println("Las soluciones son " + x1 + " y " + x2);
+		}
+		else {
+			System.out.println("No tiene solución ");				
+		}
+		
+		sc.close();
 	}
 
 }
