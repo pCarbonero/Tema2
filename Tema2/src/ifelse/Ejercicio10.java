35ca2706981d573a0a059349486fc340a426cfe3
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-05 12:10:28 +0200

    [NEW] ej 10

--- Tema2/src/ifelse/Ejercicio10.java
+++ Tema2/src/ifelse/Ejercicio10.java
@@ -1,10 +1,32 @@
 package ifelse;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Variables
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número entero ");
+		num1 = sc.nextInt();
+		System.out.println("Dime un número entero ");
+		num2 = sc.nextInt();
+		System.out.println("Dime un número entero ");
+		num3 = sc.nextInt();
+		
+		if (num1 + num2 == num3) {
+			System.out.println("La suma de dos de esos numeros da el tercero ");
+		}
+		else if (num1 + num3 == num2) {
+			System.out.println("La suma de dos de esos numeros da el segundo");
+		}
+		else if (num2 + num3 == num1) {
+			System.out.println("La suma de dos de esos numeros da el primero ");
+		}
+		
+		sc.close();
 	}
 
 }
