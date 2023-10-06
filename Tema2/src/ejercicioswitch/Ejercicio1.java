2b4fd2a512f14b0b4840720ea742d2438429ea18
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-06 14:39:24 +0200

    [NEW] ej 1

--- Tema2/src/ejercicioswitch/Ejercicio1.java
+++ Tema2/src/ejercicioswitch/Ejercicio1.java
@@ -1,10 +1,44 @@
 package ejercicioswitch;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Variables
+		int nota;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un número ");
+		nota = sc.nextInt();
+		
+		switch (nota) {
+		case 0,1,2,3,4: {
+			System.out.println("Insuficiente");
+			break;
+		}
+		case 5:{
+			System.out.println("Suficiente");
+			break;
+		}
+		case 6:{
+			System.out.println("Bien");
+			break;
+		}
+		case 7,8:{
+			System.out.println("Notable");
+			break;
+		}
+		case 9,10:{
+			System.out.println("Sobresaliente");
+			break;
+		}
+		default:
+			System.out.println("No puedes saar esa nota ");
+			break;
+		}
+		
+		sc.close();
 	}
 
 }
