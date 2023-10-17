805ee1ca141639e7879bdcdf68cd651d4b804794
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-17 13:13:16 +0200

    [NEW] ej 1

--- Tema2/src/ejercicioswhile/Ejercicio1.java
+++ Tema2/src/ejercicioswhile/Ejercicio1.java
@@ -1,10 +1,26 @@
 package ejercicioswhile;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// variables
+		int num, suma = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserte un numero entero ");
+		num = sc.nextInt();
+		
+		while (num>=0) {
+			suma += num;	
+			System.out.println("Dame otro número entero, si quieres parar introduce uno negativo ");
+			num = sc.nextInt();
+		}
+		
+		System.out.println("La suma de todos los numeros es " + suma);
+		
+		sc.close();
 	}
 
 }
