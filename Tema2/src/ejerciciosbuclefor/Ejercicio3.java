499a61544bcdb58c0027d47ff33b5e5c6d1799be
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-24 14:10:21 +0200

    [NEW] ej 3

---
+++ Tema2/src/ejerciciosbuclefor/Ejercicio3.java
@@ -0,0 +1,16 @@
+package ejerciciosbuclefor;
+
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 100; i++) {
+			//condicional que comprueba si i entre siete da de resto 0
+			if (i%7 == 0) {
+				System.out.println(i);
+			}			
+		}
+	}
+
+}
