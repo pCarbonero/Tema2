2122c1f707541893b5f917d2bbaf1e4793cf570b
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-24 14:42:49 +0200

    [UPD] ej 6 y 7

--- Tema2/src/ejerciciosbuclefor/Ejercicio6.java
+++ Tema2/src/ejerciciosbuclefor/Ejercicio6.java
@@ -4,11 +4,12 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		//variable para almacenar la suma de los numeros
-		int n = 0;
+		int n = 0, cont = 1;
 		
 		//bucle que suma a i dos de forma que solo sea impar
-		for (int i = 1; i <= 10; i+=2) {
+		for (int i = 1; cont <= 10; i+=2) {
 			n += i;// suma cada valor de i
+			cont++;
 		}
 		System.out.println(n);//imprime el resultado por la consola
 	}
---
+++ Tema2/src/ejerciciosbuclefor/Ejercicio7.java
@@ -0,0 +1,11 @@
+package ejerciciosbuclefor;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		//variable para guardar el numero que vamos a introducir y para guardar 
+		//el factorial
+		int n, factorial;
+	}
+
+}
