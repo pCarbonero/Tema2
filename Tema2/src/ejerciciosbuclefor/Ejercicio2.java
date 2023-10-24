4821b53db5b8acc278ae4233017590b0ff6f32a0
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-24 14:10:09 +0200

    [NEW] ej2

---
+++ Tema2/src/ejerciciosbuclefor/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejerciciosbuclefor;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 2; i <= 200; i += 2) {
+			System.out.println(i);;			
+		}
+	}
+
+}
