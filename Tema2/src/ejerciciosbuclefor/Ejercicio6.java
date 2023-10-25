8ffc99296e1c0fffb98d65a9242e98baad5a6d87
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-25 08:38:46 +0200

    [UPD] ej 6

--- Tema2/src/ejerciciosbuclefor/Ejercicio6.java
+++ Tema2/src/ejerciciosbuclefor/Ejercicio6.java
@@ -4,12 +4,15 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		//variable para almacenar la suma de los numeros
-		int n = 0, cont = 1;
+		int n = 0, cont = 0;
 		
 		//bucle que suma a i dos de forma que solo sea impar
-		for (int i = 1; cont <= 10; i+=2) {
+		for (int i = 1; i <= 20; i+=2) {
 			n += i;// suma cada valor de i
-			cont++;
+			cont++;//contador que suma uno cada vez que se suma un numero impar
+			if (cont == 10) {//una vez sume los 10 primeros numeros para
+				break;
+			}
 		}
 		System.out.println(n);//imprime el resultado por la consola
 	}
