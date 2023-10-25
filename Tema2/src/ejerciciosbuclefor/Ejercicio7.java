ab934b8a16a8b66884b3ed78ea953a871dc75aff
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-25 09:05:15 +0200

    [NEW] ej 7

--- Tema2/src/ejerciciosbuclefor/Ejercicio7.java
+++ Tema2/src/ejerciciosbuclefor/Ejercicio7.java
@@ -1,11 +1,24 @@
 package ejerciciosbuclefor;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
 		//variable para guardar el numero que vamos a introducir y para guardar 
 		//el factorial
-		int n, factorial;
+		int n;
+		double factorial = 1;
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Dame un número para calcular su factorial ");
+		n = sc.nextInt();		
+		
+		for (int i = n; i >= 1; i--) {
+			factorial = factorial * i;
+		}
+		System.out.println(factorial);
+		sc.close();
 	}
 
 }
