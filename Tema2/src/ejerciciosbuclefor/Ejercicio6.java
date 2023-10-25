aedeaf193a01521dc12e9c328413b99dfd7be4dc
pCarbonero <pablo.carbonero@iesnervion.es>
2023-10-25 09:04:55 +0200

    [UPD] actualizacion

--- Tema2/src/ejerciciosbuclefor/Ejercicio6.java
+++ Tema2/src/ejerciciosbuclefor/Ejercicio6.java
@@ -8,11 +8,7 @@ public class Ejercicio6 {
 		
 		//bucle que suma a i dos de forma que solo sea impar
 		for (int i = 1; i <= 20; i+=2) {
-			n += i;// suma cada valor de i
-			cont++;//contador que suma uno cada vez que se suma un numero impar
-			if (cont == 10) {//una vez sume los 10 primeros numeros para
-				break;
-			}
+			n += i;
 		}
 		System.out.println(n);//imprime el resultado por la consola
 	}
